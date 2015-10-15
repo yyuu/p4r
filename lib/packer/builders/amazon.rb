f27f7c29ef5300fae43542762dcb270bc63cba6a
Yamashita, Yuu <peek824545201@gmail.com>
2015-10-15 10:43:13 +0000

    generate ssh keypair in ssh builder

--- lib/packer/builders/amazon.rb
+++ lib/packer/builders/amazon.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require "digest/sha2"
 require "packer/builders/ssh"
 
 module Packer
@@ -7,10 +8,18 @@ module Packer
     class Amazon < Ssh
       def setup(options={})
         super
+        @amazon_security_group = "packer-#{Digest::SHA256.hexdigest(Time.new.to_s).slice(0, 16)}"
+        create_security_group(@amazon_security_group)
       end
 
       def teardown(options={})
         super
+      ensure
+        begin
+          delete_security_group(@amazon_security_group)
+        rescue => error
+          logger.warn(error)
+        end
       end
 
       def build(options={})
@@ -27,6 +36,26 @@ module Packer
 
       def run(cmdline, options={})
         super
+      end
+
+      private
+      def authorize_ssh_public_key(ssh_public_key)
+        super
+        logger.debug("register ssh public key at #{ssh_public_key.dump} to amazon")
+      end
+
+      def revoke_ssh_public_key(ssh_public_key)
+        super
+      ensure
+        logger.debug("revoke ssh public key at #{ssh_public_key.dump} from amazon")
+      end
+
+      def create_security_group(security_group)
+        logger.debug("create security group #{security_group.dump}")
+      end
+
+      def delete_security_group(security_group)
+        logger.debug("delete security group #{security_group.dump}")
       end
     end
   end
--- lib/packer/builders/ssh.rb
+++ lib/packer/builders/ssh.rb
@@ -1,8 +1,33 @@
 #!/usr/bin/env ruby
 
+require "fileutils"
+require "tmpdir"
+
 module Packer
   module Builders
     class Ssh < NullBuilder
+      def setup(options={})
+        super
+        @ssh_tmpdir = Dir.mktmpdir
+        create_ssh_keypair(@ssh_tmpdir)
+        authorize_ssh_public_key(@ssh_public_key)
+      end
+
+      def teardown(options={})
+        super
+      ensure
+        begin
+          revoke_ssh_public_key(@ssh_public_key)
+        rescue => error
+          logger.warn(error)
+        end
+        begin
+          delete_ssh_keypair(@ssh_tmpdir)
+        rescue => error
+          logger.warn(error)
+        end
+      end
+
       def hostname()
         "127.0.0.1"
       end
@@ -13,6 +38,31 @@ module Packer
 
       def run(cmdline, options={})
         logger.debug(Shellwords.shelljoin(["ssh", hostname, "--", cmdline]))
+      end
+
+      private
+      def create_ssh_keypair(tmpdir)
+        cmdline = Shellwords.shelljoin(["ssh-keygen", "-N", "", "-f", File.join(tmpdir, "identity")])
+        if system(cmdline)
+          @ssh_private_key = File.join(tmpdir, "identity")
+          @ssh_public_key = File.join(tmpdir, "identity.pub")
+          logger.debug("generated temporary ssh keypair at #{tmpdir.dump}")
+        else
+          raise("failed: #{cmdline}")
+        end
+      end
+
+      def delete_ssh_keypair(tmpdir)
+        FileUtils.rm_rf(tmpdir)
+        logger.debug("removed temporary ssh keypair at #{tmpdir.dump}")
+      end
+
+      def authorize_ssh_public_key(ssh_public_key)
+        logger.debug("authorize ssh public key at #{ssh_public_key.dump}")
+      end
+
+      def revoke_ssh_public_key(ssh_public_key)
+        logger.debug("revoke ssh public key at #{ssh_public_key.dump}")
       end
     end
   end
