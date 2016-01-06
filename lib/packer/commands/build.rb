7cbe2dc8ad07c064f41e29b3efe7543c55b59636
Yamashita, Yuu <peek824545201@gmail.com>
2016-01-06 09:40:51 +0000

    Fix cyclomatic complexity, 3

--- lib/packer/commands/build.rb
+++ lib/packer/commands/build.rb
@@ -10,38 +10,9 @@ module Packer
         parallelism = Parallel.processor_count
 
         begin
-          status = Parallel.map(templates, in_threads: parallelism) do |template|
-            begin
-              template.setup(options)
-              template.build(options)
-              true
-            rescue => error
-              if error.backtrace.is_a?(Array)
-                logger.error("#{Process.pid} : " + ([error.to_s] + error.backtrace.map { |s| "\t" + s }).join("\n"))
-              else
-                logger.error("#{Process.pid} : " + error.to_s)
-              end
-              false
-            end
-          end
-        rescue Interrupt
-          status = templates.map { false }
-        end
-
-        begin
-          Parallel.each(templates, in_threads: parallelism) do |template|
-            begin
-              template.teardown(options)
-            rescue => error
-              if error.backtrace.is_a?(Array)
-                logger.warn("#{Process.pid} : " + ([error.to_s] + error.backtrace.map { |s| "\t" + s }).join("\n"))
-              else
-                logger.warn("#{Process.pid} : " + error.to_s)
-              end
-            end
-          end
-        rescue Interrupt => error
-          STDERR.puts(error)
+          status = build_templates(templates, parallelism)
+        ensure
+          clean_templates(templates, parallelism)
         end
 
         if status.all?
@@ -50,6 +21,43 @@ module Packer
           logger.error("non-zero exit status: #{status.inspect}")
           exit(1)
         end
+      end
+
+      private
+
+      def build_templates(templates, parallelism = 1)
+        Parallel.map(templates, in_threads: parallelism) do |template|
+          begin
+            template.setup(options)
+            template.build(options)
+            true
+          rescue => error
+            if error.backtrace.is_a?(Array)
+              logger.error("#{Process.pid} : " + ([error.to_s] + error.backtrace.map { |s| "\t" + s }).join("\n"))
+            else
+              logger.error("#{Process.pid} : " + error.to_s)
+            end
+            false
+          end
+        end
+      rescue Interrupt
+        templates.map { false }
+      end
+
+      def clean_templates(templates, parallelism = 1)
+        Parallel.each(templates, in_threads: parallelism) do |template|
+          begin
+            template.teardown(options)
+          rescue => error
+            if error.backtrace.is_a?(Array)
+              logger.warn("#{Process.pid} : " + ([error.to_s] + error.backtrace.map { |s| "\t" + s }).join("\n"))
+            else
+              logger.warn("#{Process.pid} : " + error.to_s)
+            end
+          end
+        end
+      rescue Interrupt => error
+        STDERR.puts(error)
       end
     end
   end
